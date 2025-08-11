66ed8562d5fce6ded8e9485b0bc88c4d281a376c
Gaston Omar Marcote <gastonomarmarcote@gmail.com>
2025-08-11 10:17:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
-import { firebase } from '@genkit-ai/firebase/plugin';
+import { firebase } from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [
@@ -10,6 +10,5 @@ export const ai = genkit({
         // apiKey: process.env.GEMINI_API_KEY, // The Firebase plugin configured above will configure the API key for you.
     }),
   ],
-  logLevel: 'debug',
   enableTracingAndMetrics: true,
 });
